5ea217cf5de6de4efd8c6c78c3818c1945dd71b8
Brandon <brandon@bduke.dev>
2024-03-13 19:49:14 -0400

    move up a dir

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/webpages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './components/webpages/about/about.component';
+import { WorkComponent } from './components/webpages/work/work.component';
+import { ContactComponent } from './components/webpages/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
